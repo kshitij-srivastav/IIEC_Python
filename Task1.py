407f3c6bb4fa61ee9fb91bd0e5e97c0973848117
Kshitij Srivastav <63575990+kshitij-srivastav@users.noreply.github.com>
2020-08-20 17:42:33 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,18 @@
+import os
+while(true):
+    print("Hello User enter Your choice of Application you want to Run ")
+    print("1- To run Notepad or Text Editor")
+    print("2- To run the Google Chrome browser ")
+    print("3- To run the media player")
+    print("4- Enter exit to quit ")
+    x=input()
+    x=x.lower()
+    if ((("run" in x) or ("execute" in x) or ("open" in x)) or (("notepad" in x) or ("text" in x) or ("editor" in  x)))
+    os.system("notepad")
+    if((("run" in x) or ("execute" in x) or (("open" in x) or ("google" in x) or ("chrome" in x) or ("browser" in x) or ("web" in x)))
+    os.system("chrome")
+    if((("run" in x) or ("execute" in x) or ("open" in x)) or(("windows" in x) or ("media" in x) or ("player" in x)))
+    os.system("wmplayer")
+    if("exit" in x) or ("quit" in x)
+    break
+print("Thank YOu for doing the task")
