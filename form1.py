f1ae744eaa838367064a59eeac2c647ce0325f06
Tanya Chetna Vaish <63603952+TanyaChetnaVaish@users.noreply.github.com>
2020-10-04 12:58:04 +0530

    Create form1.py

---
+++ form1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import cgi
+print("content-type: text/html")
+print()
+form=cgi.FieldStorage()
+cmd=form.getvalue("n")
+print(cmd)
+import subprocess
+x=subprocess.getoutput(cmd)
+print(x)
