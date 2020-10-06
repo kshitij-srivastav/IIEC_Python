3f1db0476715c303e478683e9310a576c1b03327
Tanya Chetna Vaish <63603952+TanyaChetnaVaish@users.noreply.github.com>
2020-10-06 16:13:23 +0530

    Update form1.py

--- form1.py
+++ form1.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 import cgi
+import subprocess
 print("content-type: text/html")
 print()
 form=cgi.FieldStorage()
-cmd=form.getvalue("n")
-print(cmd)
-import subprocess
-x=subprocess.getoutput(cmd)
-print(x)
+cmd=form.getvalue("n") or "abc"
+if ("run" in cmd or "execute" in cmd) and "date" in cmd:
+  x=subprocess.getoutput("date")
+  print(x)
+elif ("run" in cmd or "execute" in cmd) and ("calendar" in cmd or "cal" in cmd):
+  x=subprocess.getoutput("cal")
+  print(x)
+else:
+  print("Enter correct command!")
+
